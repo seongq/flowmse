837f4721410cac9061f2b2a85de66d837966bb93
seongq <36037178+seongq@users.noreply.github.com>
2024-05-21 03:58:25 +0000

    ode, flowmatching, straightcfm, stochasticinterpolant, schrodingerbrdige added

--- flowmse/odes.py
+++ flowmse/odes.py
@@ -105,10 +105,10 @@ class CONDFLOW(ODE):
     """ mu_t = (1-t)x0 + ty, sigma_t = sigma_min"""
     @staticmethod
     def add_argparse_args(parser):        
-        
+        parser.add_argument("--sigma-min", type=float, default=0.05, help="The minimum sigma to use. 0.05 by default.")
         return parser
 
-    def __init__(self, sigma_min=1e-4, **ignored_kwargs):
+    def __init__(self, sigma_min=0.05, **ignored_kwargs):
         
         super().__init__()        
         self.sigma_min = sigma_min
@@ -187,3 +187,201 @@ class OTFLOW_DET(ODE):
     def der_std(self,t):
         
         return 0
+    
+    
+    
+    
+    
+@ODERegistry.register("flowmatching")
+class FLOWMATCHING(ODE):
+    #original flow matching
+    #Yaron Lipman, Ricky T. Q. Chen, Heli Ben-Hamu, Maximilian Nickel, and Matt Le. Flow matching for generative modeling. International Conference on Learning Representations (ICLR), 2023.
+    #mu_t = (1-t)x+ty, sigma_t = (1-t)sigma_min +t
+    #t범위 0<t<=1
+    @staticmethod
+    def add_argparse_args(parser):        
+        parser.add_argument("--sigma-min", type=float, default=0.05, help="The minimum sigma to use. 0.05 by default.")
+        
+        return parser
+
+    def __init__(self, sigma_min=0.05, **ignored_kwargs):
+        
+        super().__init__()        
+        self.sigma_min = sigma_min
+        
+    def copy(self):
+        return OTFLOW_DET( )
+
+    def ode(self,x,t,*args):
+        pass    
+    def _mean(self, x0, t, y):       
+        return (1-t)[:,None,None,None]*x0 + t[:,None,None,None]*y
+
+    def _std(self, t):
+
+        return (1-t)*self.sigma_min + t
+
+    def marginal_prob(self, x0, t, y):
+        return self._mean(x0, t, y), self._std(t)
+
+    def prior_sampling(self, shape, y):
+        if shape != y.shape:
+            warnings.warn(f"Target shape {shape} does not match shape of y {y.shape}! Ignoring target shape.")
+        std = self._std(torch.ones((y.shape[0],), device=y.device)) #inference시 사이즈 맞추기 위함
+        z = torch.randn_like(y)
+        
+        x_T = y + z * std[:, None, None, None]
+        return x_T, z
+
+    def der_mean(self,x0,t,y):
+        return y-x0
+        
+    def der_std(self,t):
+        
+        return 1-self.sigma_min
+    
+    
+@ODERegistry.register("straighCFM")
+class STRAIGHTCFM(ODE):
+    #Rectified flow: A marginal preserving approach to optimal transport
+    #Improving and generalizing flow-based generative models with minibatch optimal transport
+    #mu_t = (1-t)x+ty, sigma_t = sigma_min
+    #t범위 0<t<=1인데 sigma=0이면 t=0포함
+    @staticmethod
+    def add_argparse_args(parser):        
+        parser.add_argument("--sigma-min", type=float, default=0.05, help="The minimum sigma to use. 0.05 by default.")
+        
+        return parser
+
+    def __init__(self, sigma_min=0.05, **ignored_kwargs):
+        
+        super().__init__()        
+        self.sigma_min = sigma_min
+        
+    def copy(self):
+        return OTFLOW_DET( )
+
+    def ode(self,x,t,*args):
+        pass    
+    def _mean(self, x0, t, y):       
+        return (1-t)[:,None,None,None]*x0 + t[:,None,None,None]*y
+
+    def _std(self, t):
+
+        return self.sigma_min
+
+    def marginal_prob(self, x0, t, y):
+        return self._mean(x0, t, y), self._std(t)
+
+    def prior_sampling(self, shape, y):
+        if shape != y.shape:
+            warnings.warn(f"Target shape {shape} does not match shape of y {y.shape}! Ignoring target shape.")
+        std = self._std(torch.ones((y.shape[0],), device=y.device)) #inference시 사이즈 맞추기 위함
+        z = torch.randn_like(y)
+        
+        x_T = y + z * std[:, None, None, None]
+        return x_T, z
+
+    def der_mean(self,x0,t,y):
+        return y-x0
+        
+    def der_std(self,t):
+        
+        return 0.0
+    
+    
+    
+@ODERegistry.register("stochasticinterpolant")
+class STOCHASTICINTERPOLANT(ODE):
+    #Building normalizing flows with stochastic interpolants.International Conference on Learning Representations
+    #mu_t = cos(1/2 pi t) x + sin(1/2 pi t) y, sigma_t = 0
+    #t는 0에서 1까지 암거나 다됨 0<=t<=1
+    @staticmethod
+    def add_argparse_args(parser):        
+        
+        return parser
+
+    def __init__(self,  **ignored_kwargs):
+        
+        super().__init__()        
+        
+        
+    def copy(self):
+        return OTFLOW_DET( )
+
+    def ode(self,x,t,*args):
+        pass    
+    def _mean(self, x0, t, y):       
+        return torch.cos(1/2 * t * torch.pi)[:,None,None,None]*x0 + torch.sin(1/2 * t * torch.pi)[:,None,None,None]*y
+
+    def _std(self, t):
+
+        return 0
+
+    def marginal_prob(self, x0, t, y):
+        return self._mean(x0, t, y), self._std(t)
+
+    def prior_sampling(self, shape, y):
+        if shape != y.shape:
+            warnings.warn(f"Target shape {shape} does not match shape of y {y.shape}! Ignoring target shape.")
+        std = self._std(torch.ones((y.shape[0],), device=y.device)) #inference시 사이즈 맞추기 위함
+        z = torch.randn_like(y)
+        
+        x_T = y + z * std[:, None, None, None]
+        return x_T, z
+
+    def der_mean(self,x0,t,y):
+        return (-torch.sin(1/2 * t * torch.pi)[:,None,None,None]*x0 + torch.cos(1/2 * t * torch.pi)[:,None,None,None]*y)* 1/2 * torch.pi
+        
+    def der_std(self,t):
+        
+        return 0.0
+    
+    
+    
+@ODERegistry.register("SchrodingerBridge")
+class SCHRODINGERBRIDGE(ODE):
+    #Improving and generalizing flow-based generative models with minibatch optimal transport
+
+    #mu_t = (1-t)x + ty,  sigma_t = sigma \sqrt{t*1(1-t)}
+    #0<t<1
+    @staticmethod
+    def add_argparse_args(parser):        
+        parser.add_argument("--sigma", type=float, default=0.05, help="The minimum sigma to use. 0.05 by default.")
+        return parser
+
+    def __init__(self, sigma, **ignored_kwargs):
+        
+        super().__init__()        
+        self.sigma = sigma
+        
+    def copy(self):
+        return OTFLOW_DET( )
+
+    def ode(self,x,t,*args):
+        pass    
+    def _mean(self, x0, t, y):       
+        return x0 * (1-t)[:,None,None,None] + y * t[:,None,None,None]
+
+    def _std(self, t):
+
+        return self.sigma * torch.sqrt( t *(1-t))
+
+    def marginal_prob(self, x0, t, y):
+        return self._mean(x0, t, y), self._std(t)
+
+    def prior_sampling(self, shape, y, T):
+        if shape != y.shape:
+            warnings.warn(f"Target shape {shape} does not match shape of y {y.shape}! Ignoring target shape.")
+        std = self._std(T*torch.ones((y.shape[0],), device=y.device)) #inference시 사이즈 맞추기 위함
+        z = torch.randn_like(y)
+        
+        x_T = y + z * std[:, None, None, None]
+        return x_T, z
+
+    def der_mean(self,x0,t,y):
+        return y-x0
+        
+    def der_std(self,t):
+        
+        return self.sigma* (1-2*t)/(2* torch.sqrt(t*(1-t)))[:,None,None,None]
